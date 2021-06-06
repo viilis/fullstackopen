ab74caf97740e80e8b6e26df4884c22e8e78c4d3
Dogedox <viplin@utu.fi>
2021-06-06 16:47:33 +0300

    kurssitiedot init

--- osa1/fullstackopen-app/src/App.js
+++ osa1/fullstackopen-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div>
+      <h1>
+        Otsikko
+      </h1>
+      <Hello emote=":D"/>
+      <Hello emote=" :)"/>
     </div>
-  );
+  )
+}
+
+const Hello = (props) => {
+  return(
+    <>
+      <p>
+        How do you do, fellow kids? {props.emote}
+      </p>
+    </>
+  )
 }
 
 export default App;
