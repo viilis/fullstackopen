5bc8a1ab07a4e4c22592e5856217bd98025e80fe
Dogedox <viplin@utu.fi>
2021-07-10 02:43:31 +0300

    2.12 beta

--- osa2/maiden/src/App.js
+++ osa2/maiden/src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import axios from 'axios'
 import { useState,useEffect } from 'react'
 
-const Search = (props) =>{
-
-  const addSearch = () =>{
-    
-  }
+const Search = ({value,onChange}) =>{
   return(
     <div>
-      <form onSubmit={}>
-        search <input value={}/>
-      </form>
+        search <input value={value} onChange={onChange}/>
+    </div>
+  )
+}
+
+const WorldDisplay = ({data,filterstate}) =>{
+  return(
+    <div>
+      {(data.filter(c => c.name.toUpperCase().includes(filterstate.toUpperCase()))).map(fc => fc.name)}
     </div>
   )
 }
@@ -21,23 +23,23 @@ const App = () => {
   const [newData,setNewData] = useState([])
   const [newFilter,setNewFilter] = useState('')
 
-  const fetchData = () =>{
+  const axiosData = () =>{
     axios.get('https://restcountries.eu/rest/v2/all').then(
       response => {
         setNewData(response.data)
       }
     )
   }
-  useEffect(fetchData,[])
-  console.log(newData)
+  useEffect(axiosData,[])
 
-  const handleSearchChange = (search) =>{
-    
+  const handleFilterChange = (event) =>{
+    setNewFilter(event.target.value)
   }
 
   return (
     <div>
-
+      <Search value={newFilter} onChange={handleFilterChange}/>
+      <WorldDisplay data={newData} filterstate={newFilter}/>
     </div>
   )
 }
