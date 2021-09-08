68deca80b841d7e6591c1fe510fd65157342f39f
dogedox <dogedox@pop-os.localdomain>
2021-09-09 01:22:01 +0300

    5

--- osa5/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/login';
-
-const getAll = async () => {
-  const res = await axios.get(baseUrl);
-  return res.data;
-};
-
-export default { getAll };
